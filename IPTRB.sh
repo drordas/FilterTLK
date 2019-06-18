662765f77da59f0096a60016eb7204757f7ad122
José Ramón Méndez Reboredo <moncho.mendez@gmail.com>
2019-06-18 12:52:24 +0200

    IPTRB updated to generate BPF ASM code

--- IPTRB.sh
+++ IPTRB.sh
@@ -4,19 +4,25 @@ if [ "$(which tcpdump)" == "" ]
 then
    echo "Please install tcpdump package. i.e."
    echo "apt-get install tcpdump"
-   exit
+   exit 1
 fi 
 
 if [ "$(which iptables)" == "" ]
 then
    echo "Your system does not support iptables"
-   exit
+   exit 1
 fi
 
-if [ test "$(which dialog)" == "" ]
+if [ "$(which dialog)" == "" ]
 then
    echo "Please install dialog package. i.e."
    echo "apt-get install dialog"
+   exit 1
+fi
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root (use sudo or su to execute it)"
+   exit 1
 fi
 
 dialog --menu "Select interface" 0 0 0 $(ip a s | grep -v "^[[:space:]]" | sed -e "s/:[^:]*$//g" -e "s/://g" | tr "\n" " ") 2>/tmp/ans
@@ -26,7 +32,12 @@ interface=$(ip a s | grep "^$(cat /tmp/ans)" | cut -d: -f 2 | sed -e "s/^[[:spac
 dialog --inputbox "Write your BPF filter here"  0 0 2>/tmp/ans
 filter="$(cat /tmp/ans)"
 
+echo "$(tcpdump -p -ni $interface -d "$filter" )" > /tmp/ans
+
+dialog --title "BPF assembler view" --textbox /tmp/ans 0 0
+
 echo "iptables -t filter -A INPUT -m bpf --bytecode \"$(tcpdump -p -ni $interface -ddd "$filter" | tr "\n" "," | sed -e "s/,$//g" -e "s/,/,\\\\\n/g")\" -j DROP" > /tmp/ans
+echo "iptables -t filter -A INPUT -m bpf --bytecode \"$(tcpdump -p -ni $interface -ddd "$filter" | tr "\n" "," | sed -e "s/,$//g" -e "s/,/,\\\\\n/g")\" -j LOG --log-prefix \"Filter.tlk\"" >> /tmp/ans
 
 dialog --title "iptables command view" --textbox /tmp/ans 0 0
 
